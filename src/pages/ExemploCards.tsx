85bcfb2a8d4f546692e4f0ecd006b17712639565
Daniel Henrique Sell <kanee.sell@gmail.com>
2025-04-16 00:42:43 -0300

    correçao

--- src/pages/ExemploCards.tsx
+++ src/pages/ExemploCards.tsx
@@ -13,11 +13,11 @@ export default function ExemploCards(){
                 <CardsImg title="Exemplo Card 1" imgUrl={image} tamanho="w-70" buttonUrl="#">
                 Card com imagem e zoom interativo ao clicar.
                 </CardsImg>
-                <CardsImg title="Exemplo Card 1" imgUrl={image} tamanho="w-70" buttonUrl="#">
+                <CardsImg title="Exemplo Card 2" imgUrl={image} tamanho="w-70" buttonUrl="#">
                 Este card exibe uma imagem e permite um zoom personalizado ao clicar, proporcionando 
                 uma experiência visual aprimorada com navegação intuitiva.
                 </CardsImg>
-                <CardsImg title="Exemplo Card 1" imgUrl={image} tamanho="w-70" buttonUrl="#">
+                <CardsImg title="Exemplo Card 3" imgUrl={image} tamanho="w-70" buttonUrl="#">
                 Um card interativo que exibe uma imagem e inclui um sistema de zoom personalizado ao 
                 ser clicado. Ideal para destacar detalhes visuais de forma fluida e intuitiva, proporcionando 
                 uma melhor experiência ao usuário sem comprometer o design responsivo do componente.
